b678fe84df175751ee4a3cfef22e2d41b014b5d3
Esmaisiyok <74541098+Esmaisiyok@users.noreply.github.com>
2020-11-18 13:09:11 +0100

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -3,6 +3,7 @@ public class BirinciGun {
 	public static void main(String[] args) {
 		System.out.println("11:57-Birinci gun");
 		System.out.println("ikinci commmit");
+		System.out.println("10:11 - ikinci Gun");
 		 
 		
 		
