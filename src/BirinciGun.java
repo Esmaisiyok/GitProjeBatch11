7c4aea4d8b30311c4ca4ea2cab3b7e2ca99a5a38
Esmaisiyok <74541098+Esmaisiyok@users.noreply.github.com>
2020-11-18 13:44:47 +0100

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -4,8 +4,9 @@ public class BirinciGun {
 		System.out.println("11:57-Birinci gun");
 		System.out.println("ikinci commmit");
 		System.out.println("10:11 - ikinci Gun");
-		System.out.println("10:26 - ikinci gun");
-			
+		System.out.println("10:26 - ikinci gun"); 
+		System.out.println("10:35 - ikinci gun");
+	
 	}
 
 }
