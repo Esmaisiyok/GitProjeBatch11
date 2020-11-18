834372c3ecf93aa5e88ba8abb1c765606ddbf82f
Esmaisiyok <esmaisiyok91@gmail.com>
2020-11-18 20:54:13 +0100

    feature

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -11,6 +11,7 @@ public class BirinciGun {
 		System.out.println("ben buradan conflict beklemiyorum");
 		System.out.println("yepyeni bir satir");
 		System.out.println("10:26 -  feature branch");
+		System.out.println("20:52 - feature deneme ");
 
 
 	}
